9763b729d9bf5511cd64fa1c429c71496daf34d1
Tada Kenta <b1021181@fun.ac.jp>
2024-09-06 14:54:03 +0900

    サイドバーに過去画像の一覧を載せるページの追加

---
+++ src/higuma_dashboard/pages/imagelist.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+import sys
+from utils.higuma_sidebar import admin_sidebar
+
+st.title("過去の検出画像")
+
+
+admin_sidebar()
--- src/higuma_dashboard/utils/higuma_sidebar.py
+++ src/higuma_dashboard/utils/higuma_sidebar.py
@@ -4,3 +4,4 @@ def admin_sidebar():
     with st.sidebar:
         st.page_link("streamlit.py", label="ホーム", icon="🏠")
         st.page_link("pages/information.py", label="プロジェクト紹介", icon="👨‍🎓")
+        st.page_link("pages/imagelist.py", label="過去の検出画像", icon="📖")
