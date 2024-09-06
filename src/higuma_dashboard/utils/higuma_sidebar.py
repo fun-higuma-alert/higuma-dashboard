037cdcb8925464c470e46c4d68e5ef06fd97ab53
kokist <g2122031@fun.ac.jp>
2024-09-06 16:14:33 +0900

    minor change: sidebar reorder list

--- src/higuma_dashboard/utils/higuma_sidebar.py
+++ src/higuma_dashboard/utils/higuma_sidebar.py
@@ -3,5 +3,5 @@ import streamlit as st
 def admin_sidebar():
     with st.sidebar:
         st.page_link("streamlit.py", label="ホーム", icon="🏠")
-        st.page_link("pages/information.py", label="プロジェクト紹介", icon="👨‍🎓")
         st.page_link("pages/imagelist.py", label="過去の検出画像", icon="📖")
+        st.page_link("pages/information.py", label="プロジェクト紹介", icon="👨‍🎓")
